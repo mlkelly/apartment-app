2940c4b844d71aa1bc318ec74e646475e3860e37
mlkelly <maurak87@gmail.com>
2020-06-17 15:34:09 -0400

    finished pair programming

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,3 +1,33 @@
 class ApartmentsController < ApplicationController
   # add controller methods
+  set :views, "app/views/apartments"
+
+  get '/apartments' do 
+    @apartments = Apartment.all
+    erb :index
+  end
+
+  get '/apartments/new' do
+    erb :new
+  end
+
+  get '/apartments/:id' do
+    @apartment = find_apartment
+    erb :show
+  end
+
+  post '/apartments' do 
+    apartment = Apartment.create(params)
+    redirect '/apartments'
+  end
+
+  delete '/apartments/:id' do
+    apartment = find_apartment
+    apartment.destroy
+    redirect '/apartments'
+  end
+
+  def find_apartment
+    Apartment.find(params[:id])
+  end
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,3 +1,48 @@
 class TenantsController < ApplicationController
   # add controller methods
+  set :views, "app/views/tenants"
+
+  get '/tenants' do 
+    @tenants = Tenant.all
+    erb :index
+  end
+
+  post '/tenants' do 
+    tenant = Tenant.create(params)
+    redirect '/tenants'
+  end
+
+  get '/tenants/new' do
+    @apartments = Apartment.all
+    erb :new
+  end
+
+  delete '/tenants/:id' do
+    tenant = find_tenant
+    tenant.destroy
+    redirect '/tenants'
+  end
+
+  get '/tenants/:id/edit' do
+    @tenant = find_tenant
+    @apartments = Apartment.all
+
+    erb :edit
+  end
+
+  get '/tenants/:id' do
+    @tenant = find_tenant
+    erb :show
+  end
+
+  patch '/tenants/:id' do
+    @tenant = find_tenant
+    @tenant.update(params)
+
+    redirect "/tenants/#{@tenant.id}"
+  end
+
+  def find_tenant
+    Tenant.find(params[:id])
+  end
 end
